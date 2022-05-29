93764ad3b1e8d78fc5072f6c4f502c22376d35ff
Yash Lala <yashlala@gmail.com>
2022-05-29 15:28:21 -0700

    Create NPT multicast entry classes.
    Won't be able to use the regular struct. Unicast and multicast are
    conceptually and mechanistically different.

---
+++ src/route/name-prefix-table-multicast-entry.cpp
@@ -0,0 +1,55 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2014-2020,  The University of Memphis,
+ *                           Regents of the University of California,
+ *                           Arizona Board of Regents.
+ *
+ * This file is part of NLSR (Named-data Link State Routing).
+ * See AUTHORS.md for complete list of NLSR authors and contributors.
+ *
+ * NLSR is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * NLSR is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * NLSR, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "name-prefix-table-multicast-entry.hpp"
+
+#include "common.hpp"
+#include "nexthop.hpp"
+
+namespace nlsr {
+
+bool
+operator==(const NamePrefixTableMulticastEntry& lhs, const NamePrefixTableMulticastEntry& rhs)
+{
+  return lhs.getNamePrefix() == rhs.getNamePrefix();
+}
+
+bool
+operator==(const NamePrefixTableMulticastEntry& lhs, const ndn::Name& rhs)
+{
+  return lhs.getNamePrefix() == rhs;
+}
+
+std::ostream&
+operator<<(std::ostream& os, const NamePrefixTableMulticastEntry& entry)
+{
+  os << "Name: " << entry.getNamePrefix() << "\n";
+
+  for (const auto& members : entry.getMemberRouters()) {
+    os << "  Member: " << entryPtr->getDestination() << "\n";
+  }
+
+  os << entry.getNexthopList(); 
+
+  return os;
+}
+
+} // namespace nlsr
---
+++ src/route/name-prefix-table-multicast-entry.hpp
@@ -0,0 +1,76 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2014-2020,  The University of Memphis,
+ *                           Regents of the University of California,
+ *                           Arizona Board of Regents.
+ *
+ * This file is part of NLSR (Named-data Link State Routing).
+ * See AUTHORS.md for complete list of NLSR authors and contributors.
+ *
+ * NLSR is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * NLSR is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * NLSR, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef NLSR_NAME_PREFIX_TABLE_MULTICAST_ENTRY_HPP
+#define NLSR_NAME_PREFIX_TABLE_MULTICAST_ENTRY_HPP
+
+#include "test-access-control.hpp"
+#include "nexthop-list.hpp"
+
+#include <set>
+
+namespace nlsr {
+
+class NamePrefixTableMulticastEntry { 
+public: 
+  NamePrefixTableMulticastEntry(const ndn::Name& name)
+    : m_namePrefix(name) 
+  { 
+  }
+
+  const ndn::Name&
+  getNamePrefix() const
+  {
+    return m_namePrefix;
+  }
+
+  const NexthopList&
+  getNexthopList() const
+  {
+    return m_nexthopList;
+  }
+
+  const NexthopList&
+  getMemberRouters() const
+  {
+    return m_memberRouters; 
+  }
+
+private: 
+  ndn::Name m_namePrefix; 
+
+PUBLIC_WITH_TESTS_ELSE_PRIVATE:
+  std::set<ndn::Name> m_memberRouters; 
+  NexthopList m_nexthopList;
+}; 
+
+bool
+operator==(const NamePrefixTableMulticastEntry& lhs, const NamePrefixTableMulticastEntry& rhs);
+
+bool
+operator==(const NamePrefixTableMulticastEntry& lhs, const ndn::Name& rhs);
+
+std::ostream&
+operator<<(std::ostream& os, const NamePrefixTableMulticastEntry& entry);
+
+} // namespace nlsr
+
+#endif // NLSR_NAME_PREFIX_TABLE_MULTICAST_ENTRY_HPP
